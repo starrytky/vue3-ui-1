21e4c5e3c0283ada1f6ba7c5c6f8e103d2be90a0
starrytky <starrytky@qq.com>
2020-12-08 01:50:15 +0800

    Topnav component package complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,22 @@
 <template>
-    <div>
-        <div class="topnav">
-            <div class="logo">logo</div>
-            <ul class="menu">
-                    <li>菜单1</li>
-                    <li>菜单2</li>
-            </ul>
-        </div>
-        <div class="banner">
-            <h1>vue3 UI</h1>
-            <h2>一个练手的UI</h2>
-            <p class="actions">
-                <a href="#">GitHub</a>
-                <a href="#">Start</a>
-            </p>
-        </div>
+  <div>
+    <Topnav />
+    <div class="banner">
+      <h1>vue3 UI</h1>
+      <h2>一个练手的UI</h2>
+      <p class="actions">
+        <a href="#">GitHub</a>
+        <a href="#">Start</a>
+      </p>
     </div>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-    .topnav {
-        border: 1px solid red;
-    }
-</style>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: {
+    Topnav,
+  },
+};
+</script>
+<style lang="scss" scoped></style>
