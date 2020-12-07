3d2ab062ecb766f870202bd6a6a94fb8174741bc
starrytky <starrytky@qq.com>
2020-12-08 02:10:27 +0800

    doc construct

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 转换</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 按钮</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 对话框</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tab">Tab 选项卡</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>vue3 UI</h1>
       <h2>一个练手的UI</h2>
       <p class="actions">
-        <a href="#">GitHub</a>
-        <a href="#">Start</a>
+        <a href="https://github.com/starrytky/vue3-ui-1">GitHub</a>
+        <routerLink to="/doc">Start</routerLink>
       </p>
     </div>
   </div>
@@ -19,4 +19,26 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner{
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
