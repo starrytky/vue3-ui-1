b1b2419ad3235a0d2569f1dc7bf361eee2361480
starrytky <starrytky@qq.com>
2020-12-09 00:55:49 +0800

    switch animation complete

---
+++ src/lib/Switch.vue
@@ -0,0 +1,48 @@
+<template>
+  <button @click="toggle" :class="{ checked: checked }">
+    <span></span>
+  </button>
+</template>
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  },
+};
+</script>
+<style lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: black;
+  border-radius: $h/2;
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h / 2;
+  transition: left 250ms;
+}
+button.checked {
+  background: blue;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,11 @@ import { createWebHashHistory, createRouter } from 'vue-router';
 
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
-import SwitchDemo from './components/Switch.vue';
-import TabDemo from './components/Tab.vue';
-import DialogDemo from './components/Dialog.vue';
-import DocDemo from './components/Doc.vue';
-import ButtonDemo from './components/Button.vue';
+import SwitchDemo from './components/SwitchDemo.vue';
+import TabDemo from './components/TabDemo.vue';
+import DialogDemo from './components/DialogDemo.vue';
+import DocDemo from './components/DocDemo.vue';
+import ButtonDemo from './components/ButtonDemo.vue';
 
 const history = createWebHashHistory()
 export const router = createRouter({
